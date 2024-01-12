c4d856172f4e5ca83921d2b0248baee6021274f2
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-01-12 19:05:08 +0100

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const emailInput = form.elements.email;
+  const passwordInput = form.elements.password;
+
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+  } else {
+    const formData = {
+      email: emailValue,
+      password: passwordValue,
+    };
+
+    console.log(formData);
+    form.reset();
+  }
+});
