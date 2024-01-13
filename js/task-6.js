035079b6738bc3a86fcd926525d29062a7c822ed
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-01-13 20:24:08 +0100

    add task-5 and task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,33 @@
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+  if (amount >= 1 && amount <= 100) {
+    const boxes = [];
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxes.push(box);
+    }
+
+    boxesContainer.append(...boxes);
+    input.value = '';
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
